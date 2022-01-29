20381eee4fc2937ed3f4b8c6b7517ce71a1d18a7
Christoph Ortner <christophortner0@gmail.com>
2022-01-28 19:08:59 -0800

    rename functions, add write, tests

--- src/fio.jl
+++ src/fio.jl
@@ -1,22 +1,29 @@
 
 
+export load_potential, save_potential
+
+function _auto_format(fname)
+   if fname[end-3:end] == "json"
+      format = :json
+   elseif fname[end-2:end] == "zip"
+      format = :zip
+   else
+      error("Unknown filename ending; try to pass the format explicitly as a kwarg")
+   end
+   return format 
+end
+
 """
-`read_potential(fname::AbstractString; format = :auto)` : convenience
+`load_potential(fname::AbstractString; format = :auto)` : convenience
 function to load an ACE1 potential. If `format == :auto` it tries to 
 guess the format from the ending of the filename. If this fails, then 
 one can try to pass the format explicitly as a kwarg, 
 * `format = :json` for a raw json file 
 * `format = :zip` for a zipped json file that was written using `JuLIP.FIO.zip_dict`
 """
-function read_potential(fname::AbstractString; format = :auto)
+function load_potential(fname::AbstractString; format = :auto)
    if format == :auto 
-      if fname[end-3:end] == "json"
-         format = :json
-      elseif fname[end-2:end] == "zip"
-         format = :zip
-      else
-         error("Unknown filename ending")
-      end
+      format = _auto_format(fname)
    end 
    
    if format == :json
@@ -24,8 +31,25 @@ function read_potential(fname::AbstractString; format = :auto)
    elseif format == :zip
       D = unzip_dict(fname)
    else
-      error("Unknown filename ending; maybe try to explicitly pass the format")
+      error("Unknown file format")
       return nothing 
    end
    
+   return read_dict(D)
 end
+
+function save_potential(fname::AbstractString, V; format = :auto)
+   if format == :auto 
+      format = _auto_format(fname)
+   end 
+
+   D = write_dict(V)
+   if format == :json 
+      JuLIP.FIO.save_dict(fname, D)
+   elseif format == :zip 
+      JuLIP.FIO.zip_dict(fname, D)
+   else      
+      error("Unknown file format")
+   end
+end
+
