d6b8c58386257c259867b23531658378378b1539
Christoph Ortner <c.ortner@warwick.ac.uk>
2022-08-16 21:22:02 -0700

    extended committee to site energy gradients

--- src/committee.jl
+++ src/committee.jl
@@ -43,6 +43,18 @@ function committee_potential(basis::RPIBasis,
    return PIPotential(basis.pibasis, c_pi, dags, evaluator, co_c_pi)                              
 end
 
+"""
+return the coefficients for committee member i with center atom species z0.
+"""
+function get_committee_coeffs(V::PIPotential, z0::AtomicNumber, i::Integer)
+   iz0 = z2i(V, z0)
+   return [ x[i] for x in V.committee[iz0] ]
+end
+
+
+
+# ------------------------------------------------------------
+#   Site energy
 
 
 co_evaluate(V::PIPotential, Rs, Zs, z0) = 
@@ -79,4 +91,91 @@ function co_evaluate!(tmp, V::PIPotential, ::StandardEvaluator,
    return Es, co_Es
 end
 
+
+
+# ------------------------------------------------------------
+#   Site energy gradient 
+
+function co_evaluate_d(V::PIPotential, Rs, Zs, z0) 
+   T = fltype(V) 
+   NCO = ncommittee(V)
+   dV = zeros(SVector{3, T}, length(Rs))
+   co_dV = SVector(ntuple(_ -> copy(dV), NCO)...)
+   tmpd = alloc_temp_d(V, length(Rs))
+   return co_evaluate_d!(dV, co_dV, tmpd, V, Rs, Zs, z0)
+end
+
+co_evaluate_d!(dV, co_dV, tmpd, V::PIPotential, Rs, Zs, z0) =
+      co_evaluate_d!(dV, co_dV, tmpd, V, V.evaluator, Rs, Zs, z0)
+
+
+function co_evaluate_d!(dEs, co_dEs, tmpd, 
+         V::PIPotential, ::StandardEvaluator,
+         Rs::AbstractVector{JVec{T}},
+         Zs::AbstractVector{<:AtomicNumber},
+         z0::AtomicNumber) where {T}
+   assert_has_co(V)
+   iz0 = z2i(V, z0)
+   NCO = ncommittee(V)
+
+   basis1p = V.pibasis.basis1p
+   tmpd_1p = tmpd.tmpd_pibasis.tmpd_basis1p
+   Araw = tmpd.tmpd_pibasis.A
+   c = V.coeffs[iz0]
+   co_c = V.committee[iz0]
+
+   # stage 1: precompute all the A values
+   A = evaluate!(Araw, tmpd_1p, basis1p, Rs, Zs, z0)
+
+   # stage 2: compute the coefficients for the ∇A_{klm} = ∇ϕ_{klm}
+   dAco = tmpd.dAco
+   # WARNING : allocation happens here, should profile whether this matters!
+   co_dAco = zeros(SVector{NCO, eltype(dAco)}, length(dAco))
+   inner = V.pibasis.inner[iz0]
+   dAAt = tmpd.dAAt
+   fill!(dAco, 0)
+   for iAA = 1:length(inner)
+      ord = inner.orders[iAA]
+      Afwd = one(eltype(A))
+      dAAt[1] = 1
+      for α = 1:ord-1
+         Afwd *= A[inner.iAA2iA[iAA, α]]
+         dAAt[α+1] = Afwd
+      end
+      Abwd = one(eltype(A))
+      for α = ord:-1:2
+         Abwd *= A[inner.iAA2iA[iAA, α]]
+         dAAt[α-1] *= Abwd
+      end
+
+      _c = c[iAA]
+      _co_c = co_c[iAA]
+      for α = 1:ord
+         iAα = inner.iAA2iA[iAA, α]
+         dAco[iAα] += _c * dAAt[α]
+         co_dAco[iAα] += _co_c * dAAt[α]
+      end
+   end
+
+   # stage 3: get the gradients
+   fill!(dEs, zero(JVec{T}))
+   for ico = 1:NCO 
+      fill!(co_dEs[ico], zero(JVec{T}))
+   end
+   
+   dAraw = tmpd.tmpd_pibasis.dA
+   for (iR, (R, Z)) in enumerate(zip(Rs, Zs))
+      evaluate_d!(Araw, dAraw, tmpd_1p, basis1p, R, Z, z0)
+      iz = z2i(basis1p, Z)
+      zinds = basis1p.Aindices[iz, iz0]
+      for iA = 1:length(basis1p, iz, iz0)
+         dEs[iR] += real(dAco[zinds[iA]] * dAraw[zinds[iA]])
+         for ico = 1:NCO
+            co_dEs[ico][iR] += real(co_dAco[zinds[iA]][ico] * dAraw[zinds[iA]])
+         end
+      end
+   end
+
+   return dEs, co_dEs
+end
 
--- test/test_committee.jl
+++ test/test_committee.jl
@@ -2,6 +2,9 @@
 
 ##
 
+@info("""-------------------------------------------
+         Testing Committee Implementation
+-------------------------------------------------""")
 
 using ACE1, ACE1.Testing
 using Printf, Test, LinearAlgebra, JuLIP, JuLIP.Testing, Random
@@ -15,7 +18,6 @@ maxdeg = 10
 r0 = 1.0
 rcut = 3.0
 basis = ACE1.Utils.rpi_basis(; species=:X, N=3, r0=r0, maxdeg=maxdeg, rcut=rcut)
-Pr = V.pibasis.basis1p.J
 len = length(basis) 
 NCO = 12 
 co_c = Diagonal(1 ./ (1:len).^2) * randn(len, 12)
@@ -23,6 +25,7 @@ c = (sum(co_c, dims=(2,)) / NCO )[:]
 
 V = combine(basis, c)
 co_V = ACE1.committee_potential(basis, c, co_c)
+Pr = V.pibasis.basis1p.J
 
 Nat = 15
 Rs, Zs, z0 = ACE1.rand_nhd(Nat, Pr, :X)
@@ -37,4 +40,25 @@ println_slim(@test(val1 ≈ val2 ≈ val3))
 @info("check the co_evaluate is consistent")
 println_slim(@test(val3 ≈ sum(co_val)/NCO))
 
+@info("check co_evaluate_d")
+
+dEs1 = evaluate_d(V, Rs, Zs, z0)
+dEs2 = evaluate_d(co_V, Rs, Zs, z0)
+dEs3, co_dEs = ACE1.co_evaluate_d(co_V, Rs, Zs, z0)
+
+@info("check consistency of three evaluate_d")
+println_slim(@test(dEs1 ≈ dEs2 ≈ dEs3))
+
+@info("check consistency of evaluate_d with committee")
+println_slim(@test(dEs3 ≈ sum(co_dEs) / NCO ))
+
+@info("check individual committee members")
+iz0 = ACE1.z2i(V, z0)
+for i in 1:NCO
+   co_V.coeffs[iz0][:] .= ACE1.get_committee_coeffs(co_V, z0, i)
+   print_tf(@test( evaluate(co_V, Rs, Zs, z0) ≈ co_val[i] ))
+   print_tf(@test( evaluate_d(co_V, Rs, Zs, z0) ≈ co_dEs[i] ))
+end
+
+## next up total energy, forces, virials 
 
