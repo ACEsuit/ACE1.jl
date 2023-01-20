03b4d04c93f1d0140d3186a6c10a06571b1058a8
Christoph Ortner <christohortner@gmail.com>
2023-01-19 21:51:53 -0800

    finished spline radial basis and tests

--- src/polynomials/splines.jl
+++ src/polynomials/splines.jl
@@ -2,27 +2,63 @@
 module Splines 
 
 
-import Interpolations 
+import Interpolations, ACE1, ForwardDiff
+
 using Interpolations: cubic_spline_interpolation
 
-using JuLIP.Potentials: z2i, SZList
-import ACE1: alloc_B, alloc_dB, alloc_temp, alloc_temp_d, evaluate!, evaluate_d!
+using JuLIP.Potentials: z2i, i2z, SZList, cutoff
+import ACE1: alloc_B, alloc_dB, alloc_temp, alloc_temp_d, 
+             evaluate!, evaluate_d!, evaluate 
 
 # _rr = range(0.0, stop=5.0, length=10)
 # _ff = sin.(_rr)
 # const SPLINE{T} = Interpolations.Extrapolation{T, 1, ScaledInterpolation{T, 1, Interpolations.BSplineInterpolation{T, 1, OffsetArrays.OffsetVector{T, Vector{T}}, BSpline{Cubic{Line{OnGrid}}}, Tuple{Base.OneTo{Int64}}}, BSpline{Cubic{Line{OnGrid}}}, Tuple{StepRangeLen{T, Base.TwicePrecision{T}, Base.TwicePrecision{T}, Int64}}}, BSpline{Cubic{Line{OnGrid}}}, Throw{Nothing}}
 
-struct RadialSplines{T, NZ, SPLINE}
-   rcut::Matrix{T}
-   splines::Matrix{SPLINE}
+struct RadialSplines{T, NZ, SPLINE} <: ACE1.ScalarBasis{T}
+   rcut::T
+   splines::Array{SPLINE, 3}
    zlist::SZList{NZ}
 end
 
+
 Base.length(basis::RadialSplines) = size(basis.splines, 1)
+
+# --------------------- Setup codes
+
+# assuming that J has a multi-transform, but if not then the 
+# zlist can be passed in. 
+
+function RadialSplines(J; nnodes = 1_000, zlist = J.trans.zlist)
+   rcut = cutoff(J) 
+   dx = rcut/nnodes 
+   rcut += 4*dx 
+   rr = range(0.0, stop=rcut, length=nnodes)
+   NZ = length(zlist)
+   NB = length(J)
+   splines = Array{Any}(undef, NB, NZ, NZ)
+   for iz = 1:NZ, iz0 = 1:NZ
+      z = i2z(zlist, iz)
+      z0 = i2z(zlist, iz0)
+      J_zz0 = zeros(NB, nnodes)
+      for (ir, r) in enumerate(rr) 
+         J_zz0[:, ir] = evaluate(J, r, z, z0)
+      end
+      for ib = 1:NB 
+         splines[ib, iz, iz0] = cubic_spline_interpolation(rr, J_zz0[ib, :])
+      end
+   end
+
+   splines_ = identity.(splines)
+   return RadialSplines(rcut, splines_, zlist)
+end
+
+# --------------------- Evaluation codes 
+
 
 alloc_B(basis::RadialSplines{T}, args...) where {T} = zeros(T, length(basis))
 
-alloc_dB(basis::RadialSplines{T}, args...) where {T} = zeros(T, length(basis))
+alloc_dB(basis::RadialSplines{T}, ::Number) where {T} = zeros(T, length(basis))
+alloc_dB(basis::RadialSplines{T}, ::Int64) where {T} = zeros(T, length(basis))
 
 alloc_temp(basis::RadialSplines, args...) = nothing 
 
@@ -35,13 +71,16 @@ function evaluate!(B, tmp, basis::RadialSplines, r, z, z0)
    fill!(B, 0)
 
    # suppose we are outside the cutoff, then skip this
-   if basis.rcut[iz, iz0] >= r
+   if basis.rcut <= r
+      print(".")
       return B 
    end
 
    len = size(basis.splines, 1)
    @assert length(B) >= len 
 
+   # inbounds here would redice cost by ca 10%, not worth it, this is 
+   # not a bottleneck
    for n = 1:len 
       spl = basis.splines[n, iz, iz0]
       B[n] = spl(r)
@@ -57,15 +96,17 @@ function evaluate_d!(B, dB, tmpd, basis::RadialSplines, r, z, z0)
    fill!(dB, 0)
 
    # suppose we are outside the cutoff, then skip this
-   if basis.rcut[iz, iz0] >= r
+   if basis.rcut <= r
       return dB 
    end
 
    len = size(basis.splines, 1)
-   @assert length(B) >= len 
+   # @assert length(B) >= len 
 
    dr = ForwardDiff.Dual(r, 1)
 
+   # inbounds here would redice cost by ca 10%, not worth it, this is 
+   # not a bottleneck
    for n = 1:len 
       spl = basis.splines[n, iz, iz0]
       d_spl = spl(dr)
--- test/polynomials/test_splines.jl
+++ test/polynomials/test_splines.jl
@@ -1,39 +1,103 @@
 
-
+using Test 
+using ACE1.Testing: print_tf, println_slim
 using ACE1, LinearAlgebra, Interpolations, BenchmarkTools, ForwardDiff
+using ACE1.Transforms: multitransform
+using JuLIP: evaluate, evaluate!, evaluate_d, evaluate_d!
+
+##
+
+
+cutoffs = Dict(
+   (:Fe, :C) => (1.5, 5.0), 
+   (:C, :Al) => (0.7, 6.0), 
+   (:Fe, :Al) => (2.2, 4.5), 
+   (:Fe, :Fe) => (2.0, 5.0), 
+   (:Al, :Al) => (2.0, 5.0), 
+   (:C, :C) => (1.5, 5.2), 
+   (:Al, :Fe) => (1.5, 5.0)  )
+
+transforms = Dict(
+      (:Fe, :C) => PolyTransform(2, (rnn(:Fe)+rnn(:C)) / 2), 
+      (:C, :Al) => PolyTransform(2, (rnn(:Al)+rnn(:C)) / 2), 
+      (:Fe, :Al) => PolyTransform(2, (rnn(:Al)+rnn(:Fe)) / 2), 
+      (:Fe, :Fe) => PolyTransform(2, rnn(:Fe)), 
+      (:Al, :Al) => PolyTransform(2, rnn(:Al)), 
+      (:C, :C) => PolyTransform(2, rnn(:C)), 
+      (:Al, :Fe) => PolyTransform(2, (rnn(:Al)+rnn(:Fe)) / 2 + 1)  
+    )
+ 
+
+mtrans = multitransform(transforms, cutoffs=cutoffs)
+
+maxdeg = 8
+N = 3
+Pr = transformed_jacobi(maxdeg, mtrans; pcut = 2)
+
+Sr = ACE1.Splines.RadialSplines(Pr; nnodes=4_000)
+
+##
+
+zFe = AtomicNumber(:Fe)
+zC = AtomicNumber(:C)
+zAl = AtomicNumber(:Al)
+
+for ntest = 1:30 
+   r = 2.5 + 3 * rand() 
+   z = rand([zFe, zC, zAl])
+   z0 = rand([zFe, zC, zAl])
+
+   p = evaluate(Pr, r, z, z0)
+   s = evaluate(Sr, r, z, z0)
+   dp = evaluate_d(Pr, r, z, z0)
+   ds = evaluate_d(Sr, r, z, z0)
+
+   print_tf(@test norm(p - s, Inf) < 1e-8)
+   print_tf(@test norm(dp - ds, Inf) < 1e-4)
+end
+
+##
+
+# r = 2.5 + 3 * rand() 
+# z = rand([zFe, zC, zAl])
+# z0 = rand([zFe, zC, zAl])
+
+# p = evaluate(Pr, r, z, z0)
+# dp = evaluate_d(Pr, r, z, z0)
+# s = evaluate(Sr, r, z, z0)
+# ds = evaluate_d(Sr, r, z, z0)
+
+# @btime evaluate!($p, nothing, $Pr, $r, $z, $z0)
+# @btime evaluate_d!($p, $dp, nothing, $Pr, $r, $z, $z0)
+# @btime evaluate!($s, nothing, $Sr, $r, $z, $z0)
+# @btime evaluate_d!($s, $ds, nothing, $Sr, $r, $z, $z0)
 
 
 ##
 
-f = sin 
-df = cos 
+@info("Test basis evaluation")
+N = 3
+D = SparsePSHDegree()
+B1p_P = BasicPSH1pBasis(Pr; species = [:Fe, :Al, :C], D = D)
+B1p_S = BasicPSH1pBasis(Sr; species = [:Fe, :Al, :C], D = D)
 
-rr = range(0.0, stop=5.0, length=10_000)
-ff = f.(rr)
-spl = cubic_spline_interpolation(rr, ff)
+rpibasis_P = RPIBasis(B1p_P, N, D, maxdeg)
+rpibasis_S = RPIBasis(B1p_S, N, D, maxdeg)
 
-# test 
-tt = 5 * rand(100_000)
-f_tt = f.(tt)
-df_tt = df.(tt)
-spl_tt = spl.(tt)
-dspl_tt = Interpolations.gradient1.(Ref(spl), tt)
+for ntest = 1:30 
+   Nat = 12 
+   Rs = [ (2 + rand() * 3) * ACE1.Random.rand_sphere() for _ = 1:Nat ]
+   Zs = rand([zFe, zC, zAl], Nat)
+   z0 = rand([zFe, zC, zAl])
 
+   B_P = evaluate(rpibasis_P, Rs, Zs, z0)
+   B_S = evaluate(rpibasis_S, Rs, Zs, z0)
 
-@show norm(f_tt - spl_tt, Inf);
-@show norm(df_tt - dspl_tt, Inf);
+   print_tf(@test norm(B_P - B_S, Inf) / Nat < 1e-5) 
 
-## 
+   dB_P = evaluate_d(rpibasis_P, Rs, Zs, z0)
+   dB_S = evaluate_d(rpibasis_S, Rs, Zs, z0)
 
-r = rand() * 5
-@info("Performance of evaluation")
-@btime $spl($r)
+   print_tf(@test norm(dB_P - dB_S, Inf) / Nat < 1e-4)
+end
 
-@info("Performance of gradient")
-@btime Interpolations.gradient1($spl, $r)
-@info("And with FOrwardDiff")
-@btime ForwardDiff.derivative($spl, $r)
-
-@btime $spl(ForwardDiff.Dual($r, 1))
-
-df = spl(ForwardDiff.Dual(r, 1))
